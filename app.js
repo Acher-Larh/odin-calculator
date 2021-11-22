a2f4e5811db547a4aa85723bfa8f5c884142ab99
Acher-Larh <achdslayer@gmail.com>
2021-11-22 22:07:25 +0100

    wrote the display and keypad functionality, still have to work on the equals and clear buttons

--- app.js
+++ app.js
@@ -24,3 +24,57 @@ function operate(item1, item2, operator){
             return subtract(item1, item2);
     }
 }
+
+const display = document.querySelector(".display");
+
+let displayContent = display.textContent;
+
+const operatorKeys = document.querySelectorAll(".operators .operator");
+
+let operatingNumbers = {
+    items: [],
+    operators: [],
+};
+operatorKeys.forEach(key => {
+    key.addEventListener('click', function(){
+        operatingNumbers.items.push(displayContent);
+        display.textContent = `${key.textContent}`;
+        displayContent = display.textContent;
+        operatingNumbers.operators.push(displayContent);
+
+        console.log(operatingNumbers);
+    });
+})
+
+
+const keypad = document.querySelectorAll(".keypad .key");
+keypad.forEach(key => {
+    key.addEventListener('click', function() {
+        displayContent = display.textContent;
+        if (display.textContent == 0) {
+            display.textContent = " ";
+        }else if (display.textContent.includes("*")
+            || display.textContent.includes("+")
+            || display.textContent.includes("/")
+            || display.textContent.includes("-")
+            || display.textContent.includes("=")
+        ){
+            display.textContent = " ";
+            
+        }
+        display.textContent = `${display.textContent}${key.textContent}`;
+        displayContent = display.textContent;
+    } );
+});
+
+const equalsKey = document.querySelector(".key.equals");
+
+equalsKey.addEventListener('click', function (e)  {
+    const operatingOperators = operatingNumbers.operators;
+    const operatingItems = operatingNumbers.items;
+    operatingItems.forEach(digit => {
+        console.log(digit);
+    });
+    
+    console.log(e.target.textContent);
+})
