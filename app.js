402fee001b82f0d78bc057bc61b005510bfd8794
Acher-Larh <achdslayer@gmail.com>
2021-11-22 12:06:50 +0100

    wrote the operation functions and made the markup for the calculator

---
+++ app.js
@@ -0,0 +1,26 @@
+function add(item1, item2){
+    return item1 + item2;
+}
+function subtract(item1, item2){
+    return item1 - item2;
+}
+function multiply(item1, item2){
+    return item1 * item2;
+
+}
+function divide(item1, item2){
+    return item1 / item2;
+}
+
+function operate(item1, item2, operator){
+    switch(operator){
+        case "/":
+            return divide(item1, item2);
+        case "*":
+            return multiply(item1, item2);
+        case "+":
+            return add(item1, item2);
+        case "-":
+            return subtract(item1, item2);
+    }
+}
